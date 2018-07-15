0c86422110af4e1e22151320f46e32609040e70f
kujarung <rnwk34@nate.com>
2018-07-15 05:53:06 +0000

    회원가입(회원가입 링크 미지정)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180712063418) do
+ActiveRecord::Schema.define(version: 20180714105831) do
+
+  create_table "books", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
